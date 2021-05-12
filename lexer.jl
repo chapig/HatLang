0e8bf13e9eff5cc6d860cafb6a073f7db0c360d3
Luis C. Gómez <46666572+chapig@users.noreply.github.com>
2021-05-12 02:43:33 -0400

    Update lexer.jl

--- lexer.jl
+++ lexer.jl
@@ -1,13 +1,28 @@
+TEMP_SYMBOL = ""
+
 function allowed(c)
-    if contains(string(c), r"[_a-zA-Z0-9]") || c === '_' || c === '(' || c === ')'
+    if contains(string(c), r"[_a-zA-Z0-9]") || c ≡ '_' || c ≡'(' || c ≡ ')'
         return true
     else
         return false
     end
-
 end
 
-function isasymbol()
+function isasymbol(TEMP_SYMBOL, chars, indexchar)
+    
+    global TEMP_SYMBOL
+
+    for i in 1:length(chars[indexchar:end])
+        if !isspace(chars[i]) || chars[i] ≡ '=' && allowed(chars[i])
+            TEMP_SYMBOL *= chars[i]
+        else
+            return [true, TEMP_SYMBOL, i]
+            break
+        end
+    end
+
+    return [false, false, false]
+
 end
 
 function is_a_string_or_char(chars, isachar, isastring)
@@ -53,72 +68,59 @@ function is_a_string_or_char(chars, isachar, isastring)
 end
 
 function scan_string(delim, chars)
-
     chars = chars[delim:end]
     isastring = findfirst('"', chars)
     isachar = findfirst('\'', chars)
     return is_a_string_or_char(chars, isachar, isastring)
-
 end
 
 function lex(file::AbstractString)
 
-    index_temp_symbol = 0
-    temp_symbol = ""
+    global TEMP_SYMBOL
     lexr = []
     error = false
 
     open(file, "r") do io
         
         chars = read(io, String)
-        nchars = range(1, length=length(chars)) #Number of chars.
         indexchar = 1
+        temp_symbol = ""
 
         while iterate(chars, indexchar) !== nothing
 
+            global TEMP_SYMBOL
+
             if isspace(chars[indexchar])
 
-                if index_temp_symbol >= 1
-                    @warn "first temp symbol init"
-                    push!(lexr, temp_symbol)
-                    index_temp_symbol = 0
-                    temp_symbol = ""
-                end
-                
                 indexchar += 1
                 continue
-
-            elseif isletter(chars[indexchar]) || chars[indexchar] === '_' || chars[indexchar] === '(' || chars[indexchar] === ')'
-
-
-                if !isspace(chars[indexchar]) && allowed(chars[indexchar])
-                    temp_symbol *= chars[indexchar]
-                    index_temp_symbol += 1
-                    indexchar += 1  
-                    continue
-                else
-                    println("Space detected")
-                    indexchar += 1  
-                    continue
-                end
 
             elseif ispunct(chars[indexchar]) && chars[indexchar] ≡ ';' || chars[indexchar] ≡ '.'
+
                 push!(lexr, chars[indexchar])
                 indexchar += 1
                 continue
+
             elseif isnumeric(chars[indexchar])
+
                 push!(lexr, chars[indexchar])
                 indexchar += 1
                 continue
+
             elseif chars[indexchar] in "(),;=:"
+
                 push!(lexr, chars[indexchar])
                 indexchar += 1
                 continue
+
             elseif chars[indexchar] in "+-*/"
+
                 push!(lexr, chars[indexchar])
                 indexchar += 1
                 continue
+
             elseif chars[indexchar] in "'" || chars[indexchar] in '"'
+
                 string_or_char = scan_string(indexchar, chars)
                 if string_or_char ≢ nothing
                     push!(lexr, string_or_char[1])
@@ -130,16 +132,17 @@ function lex(file::AbstractString)
                     break
                 end
 
-            elseif index_temp_symbol >= 1
-                @warn "second temp symbol init"
-                push!(lexr, temp_symbol)
-                index_temp_symbol = 0
-                temp_symbol = ""
-
             else
-                error = true
-                println("Unexpected character: '$(chars[indexchar])'.")
-                break
+                simbolo = isasymbol(temp_symbol, chars, indexchar)
+                println(simbolo)
+                if simbolo[1]
+                    push!(lexr, simbolo[2])
+                    indexchar += simbolo[3]
+                    continue
+                else
+                    error = true
+                    break
+                end
             end
         end
 
@@ -149,9 +152,9 @@ function lex(file::AbstractString)
             println("Chars and unfinished lexicon will be returned (Vector) in order to be debugged.")
             return [chars, lexr]
         else
+            println("Símbolo temporal: $TEMP_SYMBOL")
             println("Last index of string scanned was: $indexchar of $(length(chars))")
             return lexr 
         end
-
     end
 end
